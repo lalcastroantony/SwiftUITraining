89a43894ea595b2e5d67970caacb9c63522a2775
Lal Castro <lal.castro@icloud.com>
2023-03-29 09:08:57 +0530

    composing complex interfaces

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -1,20 +1,38 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing the details for a landmark.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing the details for a landmark.
+ */
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    enum Tab {
+        case featured
+        case list
+    }
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
